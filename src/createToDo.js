0052bbdfdd3ee553aa19bb82755fdcbb5d81149d
Tho Tu <126043463+Tho-Tu@users.noreply.github.com>
2023-09-04 18:26:41 +1000

    Create factory function for todo object

---
+++ src/createToDo.js
@@ -0,0 +1,43 @@
+export default function createToDo(
+  title,
+  description,
+  dueDate,
+  priority,
+  completed
+) {
+  const getTitle = () => title;
+  const getDescription = () => description;
+  const getDueDate = () => dueDate;
+  const getPriority = () => priority;
+  const getCompleted = () => completed;
+
+  function updateTitle(update) {
+    title = update;
+  }
+  function updateDescription(update) {
+    description = update;
+  }
+  function updateDueDate(update) {
+    dueDate = update;
+  }
+  function updatePriority(update) {
+    priority = update;
+  }
+
+  function updateCompleted(update) {
+    completed = update;
+  }
+
+  return {
+    getTitle,
+    getDescription,
+    getDueDate,
+    getPriority,
+    getCompleted,
+    updateTitle,
+    updateDescription,
+    updateDueDate,
+    updatePriority,
+    updateCompleted,
+  };
+}
