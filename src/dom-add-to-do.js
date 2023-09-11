e3eb21335d73876bc2a43b7023710534a6d9d902
Tho Tu <126043463+Tho-Tu@users.noreply.github.com>
2023-09-11 12:12:57 +1000

    Toggle add new to do

--- src/dom-add-to-do.js
+++ src/dom-add-to-do.js
@@ -1,5 +1,38 @@
 import createToDo from "./create-to-do";
+import closeSVG from "./components/icons/close.svg";
+import addSVG from "./components/icons/add.svg";
 
 export default function toDoDom() {
-  //
+  // toggle add new project with plus button
+  const addToDoButton = document.querySelector("#add-to-do-button");
+  addToDoButton.addEventListener("click", () => {
+    toggleAddToDo();
+  });
+
+  const addToDoCard = document.querySelector(".add-to-do-card");
+  const addToDoForm = document.querySelector("#add-to-do-form");
+
+  let addToDoDisplay = false;
+  function toggleAddToDo() {
+    const addIcon = new Image();
+    addIcon.src = addSVG;
+
+    const closeIcon = new Image();
+    closeIcon.src = closeSVG;
+
+    if (addToDoDisplay === false) {
+      addToDoCard.setAttribute("style", "display: flex");
+      addToDoDisplay = true;
+      addToDoButton.textContent = "";
+      addToDoButton.appendChild(closeIcon);
+    } else {
+      addToDoCard.setAttribute("style", "display: none");
+      addToDoDisplay = false;
+      addToDoButton.textContent = "";
+      addToDoButton.appendChild(addIcon);
+      addToDoForm.reset();
+    }
+  }
+
+  // create new to do card
 }
--- src/index.js
+++ src/index.js
@@ -1,20 +1,13 @@
 import projects from "./all-projects";
 import createToDo from "./create-to-do.js";
-import createProject from "./create-project.js";
 import projectsDom from "./dom-project.js";
+import toDoDom from "./dom-add-to-do";
 
 const userProject = projects();
-console.log(
-  `index.js project name: ${userProject.getProjectArray()[0].getProjectName()}`
-);
-console.log(
-  `general project to dos: ${userProject
-    .getProjectArray()[0]
-    .getProjectToDo()[0]
-    .getTitle()}`
-);
 
 projectsDom(userProject);
+
+toDoDom();
 
 const testOne = createToDo("exercise", "cardio", "today", "medium", "false");
 testOne.updateCompleted("true");
